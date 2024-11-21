6e8024c48128fd51192cf7b86f25274f39b3cbbb
Maximiliano Palay <maximilianopalay@gmail.com>
2024-11-20 21:30:17 -0600

    Create auxiliary class for kinematics calculation, add visualization attribute for enabling/disabling visualization, move ik service to topic

--- cdpr_kinematics/cdpr_kinematics/auxiliary_math.py
+++ cdpr_kinematics/cdpr_kinematics/auxiliary_math.py
@@ -1,5 +1,83 @@
 import numpy as np
 import math
+import tf_transformations
+
+class InverseKinematics():
+
+    def __init__(self, pulley_distance_xy: float, pulley_height_top: float, pulley_height_bottom: float, effector_distance_xy: float, effector_height: float):
+        self.pulley_distance_xy = pulley_distance_xy
+        self.pulley_height_top = pulley_height_top
+        self.pulley_height_bottom = pulley_height_bottom
+        self.effector_distance_xy = effector_distance_xy
+        self.effector_height = effector_height
+        
+        # a vectors (different than figure 2.3, these go up)
+        self.a1 = np.array([self.pulley_distance_xy/2.0,self.pulley_distance_xy/2.0,self.pulley_height_bottom])
+        self.a2 = np.array([-self.pulley_distance_xy/2.0,self.pulley_distance_xy/2.0,self.pulley_height_bottom])
+        self.a3 = np.array([-self.pulley_distance_xy/2.0,-self.pulley_distance_xy/2.0,self.pulley_height_bottom])
+        self.a4 = np.array([self.pulley_distance_xy/2.0,-self.pulley_distance_xy/2.0,self.pulley_height_bottom])
+        self.a5 = np.array([self.pulley_distance_xy/2.0,self.pulley_distance_xy/2.0,self.pulley_height_top])
+        self.a6 = np.array([-self.pulley_distance_xy/2.0,self.pulley_distance_xy/2.0,self.pulley_height_top])
+        self.a7 = np.array([-self.pulley_distance_xy/2.0,-self.pulley_distance_xy/2.0,self.pulley_height_top])
+        self.a8 = np.array([self.pulley_distance_xy/2.0,-self.pulley_distance_xy/2.0,self.pulley_height_top])
+
+        # b vectors
+        self.b1 = np.array([self.effector_distance_xy/2.0,self.effector_distance_xy/2.0,-self.effector_height/2.0])
+        self.b2 = np.array([-self.effector_distance_xy/2.0,self.effector_distance_xy/2.0,-self.effector_height/2.0])
+        self.b3 = np.array([-self.effector_distance_xy/2.0,-self.effector_distance_xy/2.0,-self.effector_height/2.0])
+        self.b4 = np.array([self.effector_distance_xy/2.0,-self.effector_distance_xy/2.0,-self.effector_height/2.0])
+        self.b5 = np.array([self.effector_distance_xy/2.0,self.effector_distance_xy/2.0,self.effector_height/2.0])
+        self.b6 = np.array([-self.effector_distance_xy/2.0,self.effector_distance_xy/2.0,self.effector_height/2.0])
+        self.b7 = np.array([-self.effector_distance_xy/2.0,-self.effector_distance_xy/2.0,self.effector_height/2.0])
+        self.b8 = np.array([self.effector_distance_xy/2.0,-self.effector_distance_xy/2.0,self.effector_height/2.0])
+
+        self.r = None
+        self.R = None
+
+    def calculate(self, x, y, z, rx: float = 0.0, ry: float = 0.0, rz: float = 0.0, rw: float = 1.0):
+        """ Calculate inverse kinematics from position (x,y,z) and orientation quaternion (x,y,z,w). """
+
+        # the r vector, translation from world frame to desired ee position
+        r = np.array([x,y,z])
+
+        # rotation matrix for calculations
+        # Convert to a 4x4 transformation matrix
+        matrix = tf_transformations.quaternion_matrix([rx, ry, rz, rw])
+
+        # Extract the 3x3 rotation matrix from the 4x4 transformation matrix
+        R = matrix[:3, :3]
+
+        # calculate cable lengths
+        cable1 = np.linalg.norm(self.a1-r-R@self.b1)
+        cable2 = np.linalg.norm(self.a2-r-R@self.b2)
+        cable3 = np.linalg.norm(self.a3-r-R@self.b3)
+        cable4 = np.linalg.norm(self.a4-r-R@self.b4)
+        cable5 = np.linalg.norm(self.a5-r-R@self.b5)
+        cable6 = np.linalg.norm(self.a6-r-R@self.b6)
+        cable7 = np.linalg.norm(self.a7-r-R@self.b7)
+        cable8 = np.linalg.norm(self.a8-r-R@self.b8)
+
+        self.r = r
+        self.R = R
+
+        return cable1, cable2, cable3, cable4, cable5, cable6, cable7, cable8
+
+    def get_pulley_points(self):
+        return [self.a1, self.a2, self.a3,
+                   self.a4, self.a5, self.a6,
+                   self.a7, self.a8]
+    
+    def get_effector_points(self):
+        return [self.r + self.b1,
+                    self.r + self.R@self.b2,
+                    self.r + self.R@self.b3,
+                    self.r + self.R@self.b4,
+                    self.r + self.R@self.b5,
+                    self.r + self.R@self.b6,
+                    self.r + self.R@self.b7,
+                    self.r + self.R@self.b8]
+        
+
 
 def quaternion_from_euler(ai, aj, ak):
     ai /= 2.0
@@ -23,3 +101,5 @@ def quaternion_from_euler(ai, aj, ak):
     q[3] = cj*cc + sj*ss
 
     return q
+
+
--- cdpr_kinematics/cdpr_kinematics/cdpr_kinematics.py
+++ cdpr_kinematics/cdpr_kinematics/cdpr_kinematics.py
@@ -5,10 +5,17 @@ from geometry_msgs.msg import TransformStamped
 from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
 from cdpr_kinematics.auxiliary_math import *
 from geometry_msgs.msg import Point
+from tf2_ros import TransformBroadcaster
+from geometry_msgs.msg import TransformStamped
+from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
+from cdpr_kinematics.auxiliary_math import InverseKinematics
 from visualization_msgs.msg import Marker
 from rclpy.qos import QoSProfile, QoSDurabilityPolicy
+
 from cdpr_kinematics_interfaces.srv import IKrequest
 from cdpr_kinematics_interfaces.msg import JointCommand
+from geometry_msgs.msg import PoseStamped
+from cdpr_kinematics.auxiliary_math import InverseKinematics
 
 class CDPRKinematics(Node):
     def __init__(self, ):
@@ -19,129 +26,104 @@ class CDPRKinematics(Node):
         self.pulley_height_high = 0.95#0.965 # height of pulley at its top point
         self.pulley_height_low = 1.0-0.95 # height of pulley at its top point
         self.head_anchor_distance = 0.0450 # FOR 2D MOTION
+        self.effector_height = 0.05
+        self.visualization_enabled = True
 
-        self.tf_broadcaster = TransformBroadcaster(self)
-        self.tf_static_broadcaster = StaticTransformBroadcaster(self)
-        self.broadcast_static_transforms()
+        self.inv_kin = InverseKinematics(self.pulley_distance, self.pulley_height_high, self.pulley_height_low, self.head_anchor_distance, self.effector_height)
 
-        self.srv_ik_request = self.create_service(IKrequest, "ik_request", self.cb_ik_request)
+        self.sub_ik_request = self.create_subscription(PoseStamped, "ik_request", self.cb_ik_request, 10)
 
         self.pub_cable_lengths = self.create_publisher(JointCommand, "joint_commands", 10)
 
-        markerQoS = QoSProfile(
-            depth=10,
-            durability=QoSDurabilityPolicy.TRANSIENT_LOCAL,
-        )
-        
-        self.pub_markers = self.create_publisher(
-            Marker, "line_markers", markerQoS
-        )
-
         self.ik_transform = TransformStamped()
-
 
         self._timer = self.create_timer(0.1, self.broadcast_ee_frame)
 
-    def cb_ik_request(self, request, response):
+        self._last_pose = None
+
+
+        if self.visualization_enabled:
+            self.tf_broadcaster = TransformBroadcaster(self)
+            self.tf_static_broadcaster = StaticTransformBroadcaster(self)
+            self.broadcast_static_transforms()
+
+            markerQoS = QoSProfile(
+                depth=10,
+                durability=QoSDurabilityPolicy.TRANSIENT_LOCAL,
+            )
+            
+            self.pub_markers = self.create_publisher(
+                Marker, "line_markers", markerQoS
+            )
+            
+
+    def cb_ik_request(self, msg):
         """ Callback for the inverse kinematics service call. """
-        # get xyz from request
-        x = request.position.x
-        y = request.position.y
-        z = request.position.z
+        if self._last_pose != msg:
+            # if the last received pose and the new pose are different
+            if self._last_pose is None or self._last_pose.header.stamp <= msg.header.stamp:
+                #self._last_pose = msg
+                pass
 
-        # a vectors (different than figure 2.3, these go up)
-        a1 = np.array([self.pulley_distance/2.0,self.pulley_distance/2.0,self.pulley_height_low])
-        a2 = np.array([-self.pulley_distance/2.0,self.pulley_distance/2.0,self.pulley_height_low])
-        a3 = np.array([-self.pulley_distance/2.0,-self.pulley_distance/2.0,self.pulley_height_low])
-        a4 = np.array([self.pulley_distance/2.0,-self.pulley_distance/2.0,self.pulley_height_low])
-        a5 = np.array([self.pulley_distance/2.0,self.pulley_distance/2.0,self.pulley_height_high])
-        a6 = np.array([-self.pulley_distance/2.0,self.pulley_distance/2.0,self.pulley_height_high])
-        a7 = np.array([-self.pulley_distance/2.0,-self.pulley_distance/2.0,self.pulley_height_high])
-        a8 = np.array([self.pulley_distance/2.0,-self.pulley_distance/2.0,self.pulley_height_high])
+            cable1, cable2, cable3, cable4, cable5, cable6, cable7, cable8 = self.inv_kin.calculate(msg.pose.position.x, msg.pose.position.y, msg.pose.position.z, msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w)
 
-        # b vectors
-        b1 = np.array([self.head_anchor_distance/2.0,self.head_anchor_distance/2.0,-0.025])
-        b2 = np.array([-self.head_anchor_distance/2.0,self.head_anchor_distance/2.0,-0.025])
-        b3 = np.array([-self.head_anchor_distance/2.0,-self.head_anchor_distance/2.0,-0.025])
-        b4 = np.array([self.head_anchor_distance/2.0,-self.head_anchor_distance/2.0,-0.025])
-        b5 = np.array([self.head_anchor_distance/2.0,self.head_anchor_distance/2.0,0.025])
-        b6 = np.array([-self.head_anchor_distance/2.0,self.head_anchor_distance/2.0,0.025])
-        b7 = np.array([-self.head_anchor_distance/2.0,-self.head_anchor_distance/2.0,0.025])
-        b8 = np.array([self.head_anchor_distance/2.0,-self.head_anchor_distance/2.0,0.025])
+            # publish cable lengths
+            msg_joints = JointCommand(cable1_length = cable1, cable2_length = cable2, cable3_length = cable3, cable4_length = cable4,
+                                cable5_length = cable5, cable6_length = cable6, cable7_length = cable7, cable8_length = cable8)
+            
+            self.pub_cable_lengths.publish(msg_joints)
+            
+            self.get_logger().info("publishing joint commands")
 
-        # rotation matrix for calculations (we assume no rotation, orientation lock)
-        R = np.eye(3,3)
+            if self.visualization_enabled:
 
-        # the r vector, translation from world frame to desired ee position
-        r = np.array([x,y,z])
+                pulley_points = self.inv_kin.get_pulley_points()
+                effector_points = self.inv_kin.get_effector_points()
 
-        # calculate cable lengths
-        cable1 = np.linalg.norm(a1-r-R@b1)
-        cable2 = np.linalg.norm(a2-r-R@b2)
-        cable3 = np.linalg.norm(a3-r-R@b3)
-        cable4 = np.linalg.norm(a4-r-R@b4)
-        cable5 = np.linalg.norm(a5-r-R@b5)
-        cable6 = np.linalg.norm(a6-r-R@b6)
-        cable7 = np.linalg.norm(a7-r-R@b7)
-        cable8 = np.linalg.norm(a8-r-R@b8)
+                pulley_point_arr = []
+                for point in pulley_points:
+                    pulley_point_arr.append(Point(x = point[0], y = point[1], z = point[2]))
 
-        cable1_pulley_point = Point(x = a1[0], y = a1[1], z = a1[2])
-        cable2_pulley_point = Point(x = a2[0], y = a2[1], z = a2[2])
-        cable3_pulley_point = Point(x = a3[0], y = a3[1], z = a3[2])
-        cable4_pulley_point = Point(x = a4[0], y = a4[1], z = a4[2])
-        cable5_pulley_point = Point(x = a5[0], y = a5[1], z = a5[2])
-        cable6_pulley_point = Point(x = a6[0], y = a6[1], z = a6[2])
-        cable7_pulley_point = Point(x = a7[0], y = a7[1], z = a7[2])
-        cable8_pulley_point = Point(x = a8[0], y = a8[1], z = a8[2])
+                effector_point_arr = []
+                for point in effector_points:
+                    effector_point_arr.append(Point(x = point[0], y = point[1], z = point[2]))
 
-        cable1_head_point = Point(x = (r+b1)[0], y = (r+b1)[1], z = (r+b1)[2])
-        cable2_head_point = Point(x = (r+b2)[0], y = (r+b2)[1], z = (r+b2)[2])
-        cable3_head_point = Point(x = (r+b3)[0], y = (r+b3)[1], z = (r+b3)[2])
-        cable4_head_point = Point(x = (r+b4)[0], y = (r+b4)[1], z = (r+b4)[2])
-        cable5_head_point = Point(x = (r+b5)[0], y = (r+b5)[1], z = (r+b5)[2])
-        cable6_head_point = Point(x = (r+b6)[0], y = (r+b6)[1], z = (r+b6)[2])
-        cable7_head_point = Point(x = (r+b7)[0], y = (r+b7)[1], z = (r+b7)[2])
-        cable8_head_point = Point(x = (r+b8)[0], y = (r+b8)[1], z = (r+b8)[2])
+                marker = Marker()
+                marker.header.stamp = self.get_clock().now().to_msg()
+                marker.header.frame_id = 'world'
+                marker.id = 1
+                marker.type = Marker.LINE_LIST
+                marker.action = Marker.ADD
+                marker.scale.x = 0.01
+                marker.scale.y = 0.01
+                marker.scale.z = 0.01
 
-        marker = Marker()
-        marker.header.stamp = self.get_clock().now().to_msg()
-        marker.header.frame_id = 'world'
-        marker.id = 1
-        marker.type = Marker.LINE_LIST
-        marker.action = Marker.ADD
-        marker.scale.x = 0.01
-        marker.scale.y = 0.01
-        marker.scale.z = 0.01
+                # marker color
+                marker.color.a = 1.0
+                marker.color.r = 1.0
+                marker.color.g = 1.0
+                marker.color.b = 0.0
 
-        # marker color
-        marker.color.a = 1.0
-        marker.color.r = 1.0
-        marker.color.g = 1.0
-        marker.color.b = 0.0
+                marker_points = []
 
-        marker.points = [cable1_pulley_point, cable1_head_point, cable2_pulley_point, cable2_head_point, cable3_pulley_point, cable3_head_point, cable4_pulley_point, cable4_head_point,
-                         cable5_pulley_point, cable5_head_point, cable6_pulley_point, cable6_head_point, cable7_pulley_point, cable7_head_point, cable8_pulley_point, cable8_head_point]
-        self.pub_markers.publish(marker)
+                for i in range(len(pulley_point_arr)):
+                    marker_points.append(pulley_point_arr[i])
+                    marker_points.append(effector_point_arr[i])
 
-        # update the ik transform
-        self.ik_transform.transform.translation.x = x
-        self.ik_transform.transform.translation.y = y
-        self.ik_transform.transform.translation.z = z
-        self.ik_transform.transform.rotation.x = 0.0
-        self.ik_transform.transform.rotation.y = 0.0
-        self.ik_transform.transform.rotation.z = 0.0
-        self.ik_transform.transform.rotation.w = 1.0
-        
-        self.broadcast_ee_frame()
+                marker.points = marker_points
+                self.pub_markers.publish(marker)
 
-        response.cable_lengths = [cable1, cable2, cable3, cable4, cable5, cable6, cable7, cable8]
-        
-        if request.execute:
-            msg = JointCommand(cable1_length = cable1, cable2_length = cable2, cable3_length = cable3, cable4_length = cable4,
-                               cable5_length = cable5, cable6_length = cable6, cable7_length = cable7, cable8_length = cable8)
-            self.pub_cable_lengths.publish(msg)
-
-        return response
+                # update the ik transform
+                self.ik_transform.transform.translation.x = msg.pose.position.x
+                self.ik_transform.transform.translation.y = msg.pose.position.y
+                self.ik_transform.transform.translation.z = msg.pose.position.z
+                self.ik_transform.transform.rotation.x = 0.0
+                self.ik_transform.transform.rotation.y = 0.0
+                self.ik_transform.transform.rotation.z = 0.0
+                self.ik_transform.transform.rotation.w = 1.0
+                
+                self.broadcast_ee_frame()
+            
 
     def broadcast_ee_frame(self):
         """ Broadcast the transform from world to end effector. """
