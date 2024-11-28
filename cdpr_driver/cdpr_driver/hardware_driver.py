3734ae752cf3ec76f0ae140e14931202525175d3
Maximiliano Palay <maximilianopalay@gmail.com>
2024-11-28 10:28:12 -0600

    incorporate feedforward tensions and tension feedback into hardware driver node

--- cdpr_driver/cdpr_driver/hardware_driver.py
+++ cdpr_driver/cdpr_driver/hardware_driver.py
@@ -1,7 +1,6 @@
 import rclpy
 from rclpy.node import Node
 import serial
-
 from cdpr_kinematics_interfaces.msg import JointCommand
 
 class CDPRHardware(Node):
@@ -13,47 +12,54 @@ class CDPRHardware(Node):
             exit()
         
         self.create_subscription(JointCommand, "joint_commands", self.cb_joint_command, 10)
-        self.create_timer(0.025, self.timer_cb)
+        self.create_timer(0.01, self.timer_cb)
         self.pub_feedback = self.create_publisher(JointCommand, "cable_lengths", 10)
-        #self.homing()
 
         # Lambda function to convert the incoming string to an array of floats
-        self.convert_to_floats = lambda s: list(map(float, s[1:].strip().split(',')))
+        self.convert_to_floats = lambda s: list(map(float, s[2:].strip().split(',')))
         self.cable_lengths = [-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0]
+        self.torques = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        self.new_data = False
 
     def timer_cb(self,):
         if self.serial is not None:
-            recv_str = self.serial.readline().decode()
-            if recv_str != "":
-                if recv_str[0] == "f":
-                    try:
-                        self.cable_lengths = self.convert_to_floats(recv_str)
-                    except Exception as e:
-                        self.get_logger().debug(str(e))
-                        return
-                    msg = JointCommand(cable_lengths = self.cable_lengths, cable_tensions = [])
-                    self.pub_feedback.publish(msg)
+            recv_str = None
+            while recv_str != "":
+                recv_str = self.serial.readline().decode()
+                if recv_str != "":
+                    if recv_str[0:2] == "fp":
+                        try:
+                            self.cable_lengths = self.convert_to_floats(recv_str)
+                        except Exception as e:
+                            self.get_logger().debug(str(e))
+                            return
+                        self.new_data = True
+                    elif recv_str[0:2] == "ft":
+                        try:
+                            self.torques = self.convert_to_floats(recv_str)
+                        except Exception as e:
+                            self.get_logger().debug(str(e))
+                            return
+                        self.new_data = True            
+        if self.new_data:
+            msg = JointCommand(cable_lengths= self.cable_lengths, cable_tensions = self.torques)
+            self.pub_feedback.publish(msg)
+            self.new_data = False
 
     def initialize_serial(self):
         try:
-            self.serial = serial.Serial('/dev/ttyACM0', baudrate=250000, timeout=0.0)
+            self.serial = serial.Serial('/dev/ttyACM0', baudrate=500000, timeout=0.0)
+            self.serial.set_low_latency_mode(True)
         except Exception as e:
             self.get_logger().error(str(e))
             return False
         return True
     
     def cb_joint_command(self, msg):
-        self.get_logger().info(f"sending lengths: {msg.cable1_length:.4f}, {msg.cable2_length:.4f}, {msg.cable3_length:.4f}, {msg.cable4_length:.4f}, {msg.cable5_length:.4f}, {msg.cable6_length:.4f}, {msg.cable7_length:.4f}, {msg.cable8_length:.4f}")
-        self.serial.write(f"m {msg.cable1_length} {msg.cable2_length} {msg.cable3_length} {msg.cable4_length} {msg.cable5_length} {msg.cable6_length} {msg.cable7_length} {msg.cable8_length}\\n".encode())
+        self.get_logger().info(f"sending lengths: {msg.cable_lengths[0]:.4f}, {msg.cable_lengths[1]:.4f}, {msg.cable_lengths[2]:.4f}, {msg.cable_lengths[3]:.4f}, {msg.cable_lengths[4]:.4f}, {msg.cable_lengths[5]:.4f}, {msg.cable_lengths[6]:.4f}, {msg.cable_lengths[7]:.4f}")
+        self.get_logger().info(f"sending tensions: {msg.cable_tensions[0]:.4f}, {msg.cable_tensions[1]:.4f}, {msg.cable_tensions[2]:.4f}, {msg.cable_tensions[3]:.4f}, {msg.cable_tensions[4]:.4f}, {msg.cable_tensions[5]:.4f}, {msg.cable_tensions[6]:.4f}, {msg.cable_tensions[7]:.4f}")
+        self.serial.write(f"t {msg.cable_lengths[0]:.6f} {msg.cable_lengths[1]:.6f} {msg.cable_lengths[2]:.6f} {msg.cable_lengths[3]:.6f} {msg.cable_lengths[4]:.6f} {msg.cable_lengths[5]:.6f} {msg.cable_lengths[6]:.6f} {msg.cable_lengths[7]:.6f} {msg.cable_tensions[0]:.6f} {msg.cable_tensions[1]:.6f} {msg.cable_tensions[2]:.6f} {msg.cable_tensions[3]:.6f} {msg.cable_tensions[4]:.6f} {msg.cable_tensions[5]:.6f} {msg.cable_tensions[6]:.6f} {msg.cable_tensions[7]:.6f}\\n".encode())
         self.serial.flush()
-        return
-
-    def homing(self):
-        self.get_logger().info('homing')
-        self.serial.write(b'h\n')
-        # flush?
-        #answer = self.serial.readline().decode()
-        #self.get_logger().info(answer)
         return
     
 def main():
